340d8821d0d5a8140451a2874a6ca46986073e9a
Kuba Wojdas <kubaw9@onet.eu>
2024-05-24 04:16:44 +0200

    Add yaml mock data, add yaml format test, add yaml len test

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,14 @@ import pytest
 
 people_url = "http://www.people.com"
 planets_url = "http://www.planets.com"
-mock_script_run_data = []
+mock_request_data = []
 
 
 for n in range(0, 10):
-    mock_script_run_data.append([
-        (planets_url, {"name": f"test_run{n}", "terrain": "test_data"}),
-        (people_url, {"name": f"test_run{n}", "height": "test_data"})
+    mock_request_data.append([
+        (people_url, {"name": f"test_run{n}", "height": "test_data"}),
+        (planets_url, {"name": f"test_run{n}", "terrain": "test_data"})
     ])
+
+mock_yaml_data = {"people": [{"name": f"test_run{n}", "height": "test_data"} for n in range(0, 10)],
+                  "planets": [{"name": f"test_run{n}", "terrain": "test_data"} for n in range(0, 10)]}
--- tests/test_script.py
+++ tests/test_script.py
@@ -3,11 +3,11 @@ import os
 import aiohttp
 import pytest
 
-from source.script import start_script, load_data_from_request, load_url, OUTPUT_PATH
+from source.script import start_script, load_data_from_request, load_url, load_yaml_data, yaml_data_lengths, OUTPUT_PATH
 import unittest.mock as mock
 
 from source.exceptions import IncorrectURLException, MissingDataTypeException, ZeroCountException
-from tests.conftest import mock_script_run_data as mock_data_values
+from tests.conftest import mock_request_data as mock_data_values, mock_yaml_data
 
 
 class TestScript:
@@ -48,12 +48,38 @@ class TestScript:
             with pytest.raises(MissingDataTypeException):
                 await load_url("wrong_data_type", session)
 
+    @mock.patch("source.script.COUNT_OF_PEOPLE_AND_PLANET", len(mock_yaml_data["people"]))
+    @mock.patch("source.script.load_async_tasks", side_effect=mock_data_values)
+    @pytest.mark.asyncio
+    async def test_correct_yaml_format(self, load_data_mock):
+        interval_time_value = 0
+        await start_script(interval_time_value)
+        yaml_data = load_yaml_data()
+        assert yaml_data == mock_yaml_data
+        assert isinstance(yaml_data, dict)
+        assert isinstance(yaml_data["people"], list)
+        assert isinstance(yaml_data["planets"], list)
+        for person in (yaml_data["people"]):
+            assert isinstance(person, dict)
+            assert person.get("height")
+            assert isinstance(person["height"], str)
+            assert person.get("name")
+            assert isinstance(person["name"], str)
+        for planet in (yaml_data["planets"]):
+            assert isinstance(planet, dict)
+            assert planet.get("name")
+            assert isinstance(planet["name"], str)
+            assert planet.get("terrain")
+            assert isinstance(planet["terrain"], str)
 
-    # @mock.patch("requests.get")
-    # def test_load_data_person(self, api_response_mock):
-    #     return_data = {"name": "test_person", "height": 160}
-    #     api_response_mock.return_value.json.return_value = return_data
-    #     assert load_data("person") == return_data
+    @mock.patch("source.script.COUNT_OF_PEOPLE_AND_PLANET", len(mock_yaml_data["people"]))
+    @mock.patch("source.script.load_async_tasks", side_effect=mock_data_values)
+    @pytest.mark.asyncio
+    async def test_correct_yaml_length(self, load_data_mock):
+        interval_time_value = 0
+        await start_script(interval_time_value)
+        assert yaml_data_lengths("people") == len(mock_yaml_data["people"])
+
 
 
 
