3feaa786c43f9ab003bcc61cda2307f4aa6dd25c
Kuba Wojdas <kubaw9@onet.eu>
2024-05-24 05:41:41 +0200

    modified config path logic

--- source/config.py
+++ source/config.py
@@ -7,8 +7,8 @@ with open('../config.json') as config_file:
 if not config_data["output_path"]:
     OUTPUT_PATH = os.getenv("PYTHONPATH")
 else:
-    OUTPUT_PATH = f"{config_data['output_path']}\\data.yaml"
-    
+    OUTPUT_PATH = config_data['output_path']
+OUTPUT_PATH = os.path.join(OUTPUT_PATH, 'data.yaml')
 PLANET_RANGE = config_data["max_person"]
 PERSON_RANGE = config_data["max_planets"]
 COUNT_OF_PEOPLE_AND_PLANET = config_data["count_of_people_and_planet"]
